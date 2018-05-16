4e9cb30362f2558f090460cd3f549b5472e8e06d
aditya <aditya.ghag@wohlig.com>
2018-05-16 13:30:33 +0530

    port

--- config/env/production.js
+++ config/env/production.js
@@ -25,8 +25,8 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 1337,
-  realHost: "http://wohlig.io:1337",
+  port: 82,
+  realHost: "http://wohlig.io:82",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com"]
 
   /***************************************************************************
